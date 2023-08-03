91aa1222eb30f7e449052728b98b7b53a55fad38
varun <varun@LAPTOP-68AHC6HN>
2023-08-03 14:04:13 +0530

    Add project files.

---
+++ ado.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ado.aspx.cs" Inherits="adodemo.ado" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>login page</title>
+     <style>
+    .center{
+            margin: auto;
+            width: 50%;
+            border: 5px solid black;
+            padding: 10px;
+            background-color: turquoise;
+			}
+    </style>
+</head>
+
+<body>
+    
+    <form id="form1" runat="server">
+        <div class="center">
+        <div style="margin-left: 80px; font-size: x-large;">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Login Authentication</div>
+            <p>
+            &nbsp;
+            <asp:Label ID="Label1" runat="server" Font-Bold="True" Text="USERNAME"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"  Width="266px"></asp:TextBox>
+        </p>
+        <p>
+&nbsp;<asp:Label ID="Label2" runat="server" Font-Bold="True" type="password" Text="PASSWORD"></asp:Label>
+&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server"  Width="267px"></asp:TextBox>
+        </p>
+        <div style="margin-left: 120px">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" Text="login" Width="59px" OnClick="Button1_Click" />
+        </div>
+      </div>
+
+    </form>
+</body>
+</html>
---
+++ ado.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Drawing;
+
+
+namespace adodemo
+{
+    public partial class ado : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-68AHC6HN\\SQLEXPRESS;Initial Catalog=login_credentials;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("select password from login where username=@password", con);
+            SqlParameter sp = new SqlParameter();
+            sp.ParameterName = "password";
+            sp.Value = TextBox1.Text.ToLower();
+            cmd.Parameters.Add(sp);
+            if (TextBox1.Text == "" || TextBox2.Text == "")
+            {
+                Response.Write("Please enter both username and passwords");
+            }
+            else
+            {
+
+                try
+                {
+                    con.Open();
+                    string passw = Convert.ToString((cmd.ExecuteScalar()));
+                    if (TextBox2.Text.ToLower() == passw)
+                    {
+                        Response.Write("login succesful");
+                    }
+                    else
+                    {
+                        string ss = "Invalid";
+                        Response.Write(ss);
+                            
+                       
+                    }
+
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+                    con.Close();
+                }
+            }
+        }
+    }
+}
+    
+
+      
